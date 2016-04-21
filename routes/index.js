b60cae534cf0cec090de78802fba1c769f6120b7
sinclabs <subbalam123@yahoo.co.in>
2016-04-21 14:30:58 +0200

    Remove DB

--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,22 @@
 //Reqire everything here
 var express = require('express');
 var router = express.Router();
-var db = require("../local_modules/db");
+//var db = require("../local_modules/db");
+var posts = require("../public/AppData/Data.json");
 
 
 //When home page is requested
 router.get('/', function(req, res, next) {
-  db.posts.find(function(err, posts) {
-    if( err || !posts) {
-      console.log("No post found");
-      res.render('404');
-    }
-    else {
-      res.render('index', { posts: JSON.stringify(posts), Page: "index" });
-    }
-  });
+  // db.posts.find(function(err, posts) {
+  //   if( err || !posts) {
+  //     console.log("No post found");
+  //     res.render('404');
+  //   }
+  //   else {
+  //     res.render('index', { posts: JSON.stringify(posts), Page: "index" });
+  //   }
+  // });
+  res.render('index', { posts: JSON.stringify(posts), Page: "index" });
 });
 
 
